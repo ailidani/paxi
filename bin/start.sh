533374085a518ddae3adb4cf46fe94afa9ac420f
ailidani <ailidani@Ailidanis-MBP.fios-router.home>
2017-12-10 14:48:01 -0500

    log here

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 PID_FILE=server.pid
 
-./server -log_dir=logs -log_level=info -sid $1 -nid $2 -master $3 &
+./server -log_dir=. -log_level=info -sid $1 -nid $2 -master $3 &
 echo $! >> ${PID_FILE}
