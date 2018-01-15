fd32cbc027b00171dd0dda38e8f4e88ab105cf58
ailidani <ailidani@Ailidanis-MBP.fios-router.home>
2018-01-15 12:25:57 -0500

    update

--- wankeeper/replica.go
+++ wankeeper/replica.go
@@ -1,24 +1,32 @@
 package wankeeper
 
 import (
-	. "github.com/ailidani/paxi"
+	"github.com/ailidani/paxi"
+	"github.com/ailidani/paxi/paxos"
 )
 
 type Replica struct {
-	*Node
-	leader bool
-	master bool
+	paxi.Node
+	*paxos.Paxos
+	// current levels are 1 and 2
+	level int
 }
 
-func NewReplica(config Config) *Replica {
+func NewReplica(config paxi.Config) *Replica {
 	r := new(Replica)
-	r.Node = NewNode(config)
+	r.Node = paxi.NewNode(config)
+	r.Paxos = paxos.NewPaxos(r)
 	if config.ID.Zone() == 1 {
-		r.master = true
+		r.level = 2
 	}
-	r.Register(Request{}, r.handleRequest)
+	r.Register(paxi.Request{}, r.handleRequest)
 	return r
 }
 
-func (r *Replica) handleRequest(msg Request) {
+func (r *Replica) handleRequest(msg paxi.Request) {
+}
+
+// Broadcast overrides Socket interface in Node
+func (r *Replica) Broadcast(msg interface{}) {
+	r.Node.Multicast(r.ID().Zone(), msg)
 }
