505843c928cb506e960e68b358acc6eec4571ef7
ailidani <ailidani@eddie.fios-router.home>
2018-02-04 17:13:33 -0500

    reset time for every interval

--- wpaxos2/stat.go
+++ wpaxos2/stat.go
@@ -26,19 +26,21 @@ func newStat(interval int) *stat {
 func (s *stat) hit(id paxi.ID) paxi.ID {
 	s.hits[id]++
 	s.sum++
+	var res paxi.ID
 	if time.Since(s.time) >= time.Millisecond*time.Duration(s.interval) {
 		for id, n := range s.hits {
 			if n >= s.sum/2 {
-				// TODO should we reset for every interval?
-				s.reset()
-				return id
+				res = id
 			}
 		}
+		// we reset for every interval
+		s.reset()
 	}
-	return ""
+	return res
 }
 
 func (s *stat) reset() {
 	s.hits = make(map[paxi.ID]int)
 	s.sum = 0
+	s.time = time.Now()
 }
