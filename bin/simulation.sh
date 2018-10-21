cfedea0bd5bb3f232b012eab98bc1e3f22f9b440
ailidani <eddie.aili@gmail.com>
2018-10-21 00:36:31 -0700

    node level fast read

--- bin/simulation.sh
+++ bin/simulation.sh
@@ -9,15 +9,8 @@ if [ -z "${PID}" ]; then
     go build ../server/
     go build ../client/
     go build ../cmd/
-    ./server -sim=true -log_dir=logs -log_level=info &
+    ./server -sim=true -log_dir=logs -log_level=debug &
     echo $! >> ${PID_FILE}
-    sleep 3
-    ./client -id=1.1 -log_level=debug > c1 &
-    echo $! >> ${PID_FILE}
-    # ./client -id=2.1 -log_level=info > c2 &
-    # echo $! >> ${PID_FILE}
-    # ./client -id=3.1 -log_level=info > c3 &
-    # echo $! >> ${PID_FILE}
 else
     echo "Servers are already started in this folder."
     exit 0
